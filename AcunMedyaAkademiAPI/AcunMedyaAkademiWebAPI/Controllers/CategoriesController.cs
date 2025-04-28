fe0266736ab52e731693ca2bebb053ac7c2ca67f
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-04-28 21:09:30 +0300

    28.04.2025 - Adım 4

---
+++ AcunMedyaAkademiAPI/AcunMedyaAkademiWebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using AcunMedyaAkademiWebAPI.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcunMedyaAkademiWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly WebApiDbContext _context;
+        public CategoriesController(WebApiDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var categories = _context.Categories.ToList();
+            return Ok(categories);
+        }
+    }
+}
--- AcunMedyaAkademiAPI/AcunMedyaAkademiWebAPI/Program.cs
+++ AcunMedyaAkademiAPI/AcunMedyaAkademiWebAPI/Program.cs
@@ -1,7 +1,9 @@
+using AcunMedyaAkademiWebAPI.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<WebApiDbContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
