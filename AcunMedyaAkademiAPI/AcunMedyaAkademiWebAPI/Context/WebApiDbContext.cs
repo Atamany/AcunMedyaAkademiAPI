56557efc5b3a680295832a9724d2a40bb70cad55
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-04-28 21:00:14 +0300

    28.04.2025 - Adım 3

---
+++ AcunMedyaAkademiAPI/AcunMedyaAkademiWebAPI/Context/WebApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AcunMedyaAkademiWebAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcunMedyaAkademiWebAPI.Context
+{
+    public class WebApiDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=YIGITATAMANPC;initial catalog=AcunMedyaAkademiDB;integrated Security=True; TrustServerCertificate=True");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- AcunMedyaAkademiAPI/AcunMedyaAkademiWebAPI/Entities/Product.cs
+++ AcunMedyaAkademiAPI/AcunMedyaAkademiWebAPI/Entities/Product.cs
@@ -4,6 +4,10 @@
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
 
     }
 }
