a67acca673d34d5285abe5a3e7a0f899f115cfae
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-05-02 21:10:38 +0300

    02.05.2025 - Adım 1

---
+++ AcunMedyaAkademiWebUI/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using AcunMedyaAkademiWebUI.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace AcunMedyaAkademiWebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7163/api/Categories");
+            if(response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
