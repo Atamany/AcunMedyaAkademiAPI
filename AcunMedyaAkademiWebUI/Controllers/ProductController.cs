f5e17ec47f2d45eb9cd84abf2908d49f04cf9cd3
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-05-07 21:27:53 +0300

    07.05.2025 - Adım 4

---
+++ AcunMedyaAkademiWebUI/Controllers/ProductController.cs
@@ -0,0 +1,80 @@
+﻿using AcunMedyaAkademiWebUI.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AcunMedyaAkademiWebUI.Controllers
+{
+
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7163/api/Products");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDTO model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            var content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7163/api/Products", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateProduct(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7163/api/Products/" + id);
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<UpdateProductDTO>(jsonData);
+            return View(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDTO model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            var content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:7163/api/Products/", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.DeleteAsync("https://localhost:7163/api/Products/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ AcunMedyaAkademiWebUI/DTOs/UpdateProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace AcunMedyaAkademiWebUI.DTOs
+{
+    public class UpdateProductDTO
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
